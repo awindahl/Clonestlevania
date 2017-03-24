89e6277342edc2021865c5c5610ae8aeb5391e19
Awindahl <awindahl@kth.se>
2017-03-24 16:20:04 +0100

    fixed coin bug

--- Assets/Scripts/player_attack1.gd
+++ Assets/Scripts/player_attack1.gd
@@ -1,13 +1,8 @@
 
-extends RigidBody2D
+extends RayCast2D
 
 var this
 
 func _ready():
 	this = get_node(".")
 	this.set_meta("Type" , "Attack")
-	set_mode(1)
-
-func _on_body_enter(other):
-	if other.get_meta("Type") == "Enemy":
-		other.free()
